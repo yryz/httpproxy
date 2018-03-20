881b94c49dbe8a23ee9826d7d36471d29f04cce4
yryz <yryznet@qq.com>
2018-03-20 10:12:50 +0800

    增加raspberrypi arm平台编译

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ tar zcvf httpproxy-win64.tar.gz httpproxy
 
 GOOS=windows GOARCH=386 go build -ldflags "-w -s"
 tar zcvf httpproxy-win32.tar.gz httpproxy
+
+GOOS=linux GOARCH=arm go build -ldflags "-w -s"
+tar zcvf httpproxy-linux_arm.tar.gz httpproxy
+
